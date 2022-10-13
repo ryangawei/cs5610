f620d804ba33c56b2e253a024c8f912064822614
Guoao Wei <guoao.wei@outlook.com>
2022-10-13 17:19:46 -0400

    Populate array practice week 5

--- week5/scripts/main.js
+++ week5/scripts/main.js
@@ -1,23 +1,36 @@
-function getNumber() {
-    let radius;
-    do {
-        radius = window.prompt(
-            "Please enter the radius:"
-        );
-    } while (isNaN(radius));
-    return radius;
+// function getNumber() {
+//     let radius;
+//     do {
+//         radius = window.prompt(
+//             "Please enter the radius:"
+//         );
+//     } while (isNaN(radius));
+//     return radius;
+// }
+
+// let radius = getNumber();
+
+// function calculateArea(radius) {
+//     return Math.PI * (radius**2);
+// }
+
+// let area = calculateArea(radius);
+
+// const radius_p = document.querySelector("#radius");
+// const result_p = document.querySelector("#result");
+
+// radius_p.textContent += ` ${radius}`;
+// result_p.textContent += ` ${area}`;
+
+let shoppingListElement = document.querySelector(".shopping");
+let shoppingItems = ["bread", "cheese", "pepper"];
+
+function populateList(arr) {
+    for (let item of shoppingItems) {
+        let li = document.createElement("li");
+        li.innerText = item;
+        shoppingListElement.appendChild(li);
+    }
 }
 
-let radius = getNumber();
-
-function calculateArea(radius) {
-    return Math.PI * (radius**2);
-}
-
-let area = calculateArea(radius);
-
-const radius_p = document.querySelector("#radius");
-const result_p = document.querySelector("#result");
-
-radius_p.textContent += ` ${radius}`;
-result_p.textContent += ` ${area}`;
+populateList(shoppingItems);
