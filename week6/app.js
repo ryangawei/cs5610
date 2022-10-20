9cbe657bd7a47098b1e01f3cf0cee547ad2fe5e4
Guoao Wei <guoao.wei@outlook.com>
2022-10-20 16:28:59 -0700

    Add router

--- week6/app.js
+++ week6/app.js
@@ -8,6 +8,30 @@
 //     });
 // });
 
-const logger = require("./logger.js")
-logger.log();
-console.log(logger.version);
+// const logger = require("./logger.js")
+// logger.log();  
+// console.log(logger.version);
+
+const express = require("express");
+const router = require("./routes/tasks");
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use("/tasks", router);
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.send('Hello World')
+})
+
+app.get('/users', (req, res) => {
+  // console.log(req.params);
+  res.send(`You've sent the query ${JSON.stringify(req.query)}`);
+})
+  
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
