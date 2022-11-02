9020f3b8b8c275440ff0df6b27d9c3c228382e76
Guoao Wei <guoao.wei@outlook.com>
2022-11-01 21:05:06 -0700

    Week 8 class, CRUD with MongoDB and integrated with express

--- week6/app.js
+++ week6/app.js
@@ -15,30 +15,30 @@ const util = require("util");
 //   }
 // });
 
-const readFilePromise = util.promisify(fs.readFile);
-const writeFilePromise = util.promisify(fs.writeFile);
+// const readFilePromise = util.promisify(fs.readFile);
+// const writeFilePromise = util.promisify(fs.writeFile);
 
-writeFilePromise("message.txt", "Hello")
-  .then(() => {
-    return readFilePromise("message.txt", { encoding: "utf-8" });
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// writeFilePromise("message.txt", "Hello")
+//   .then(() => {
+//     return readFilePromise("message.txt", { encoding: "utf-8" });
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-async function testAsyncAwait() {
-  try {
-    await writeFilePromise("message.txt", "Hello");
-    const data = await readFilePromise("message.txt", { encoding: "utf-8" });
-    console.log(data)
-  } catch (err) {
-    console.log(err);
-  }
-}
-testAsyncAwait();
+// async function testAsyncAwait() {
+//   try {
+//     await writeFilePromise("message.txt", "Hello");
+//     const data = await readFilePromise("message.txt", { encoding: "utf-8" });
+//     console.log(data)
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// testAsyncAwait();
 // const logger = require('./logger.js');
 // // console.log(logger)
 // logger.logFunction();
@@ -46,17 +46,29 @@ testAsyncAwait();
 const express = require("express");
 const router = require("./routes/tasks");
 
+// const db = require("./db");
+// console.log(db);
+// NOTE: Playing with MongoDB 
+const {connectToDB, addToDB} = require("./db");
+
 const app = express();
 const port = 3000;
+// Middleware that parse string into json 
+app.use(express.json());
+app.use(express.urlencoded());
+// Set views folder
 app.set("views", "views");
+// Set template 
 app.set("view engine", "pug");
+// Set static files folder
 app.use(express.static("public"));
-app.use("/tasks", router);
+app.use("/api/tasks", router);
 
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`application is listening on port ${port}`);
+  await connectToDB();
 });
