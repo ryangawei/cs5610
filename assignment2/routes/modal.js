9fb187f64356496b1aaab97edfe9c62d59c51823
Guoao Wei <guoao.wei@outlook.com>
2022-11-04 20:47:59 -0700

    Update backend to include new view

--- assignment2/routes/modal.js
+++ assignment2/routes/modal.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {pretty: true, items: [{name: "Jack"}]});
+  res.render('modal', {pretty: true, items: [{name: "Jack"}]});
 });
 
 module.exports = router;
