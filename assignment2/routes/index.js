7a2b284c64e69445d6163bc4911eead0d7a878fc
Guoao Wei <guoao.wei@outlook.com>
2022-11-04 22:16:01 -0700

    Read cat data from db and display

--- assignment2/routes/index.js
+++ assignment2/routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require("../public/javascripts/db");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {pretty: true, items: [{name: "Jack"}]});
+router.get('/', async function(req, res, next) {
+  // Display cat data from db
+  const data = await db.readAll();
+
+  const items = [];
+
+  const now = new Date();
+  let i = 0;
+  for (const doc of data) {
+    const sex = doc.sex[0].toUpperCase() + doc.sex.slice(1);
+
+    let age = now.getFullYear() - doc.date_of_birth.getFullYear();
+    
+    if (age > 0) {
+      age = `${age} years old`;
+    }
+    else {
+      age = now.getMonth() - doc.date_of_birth.getMonth();
+      age = `${age} months old`;
+    }
+
+    item = {
+      _id: doc._id,
+      card_id: i,
+      name: doc.name,
+      sex: sex,
+      age: age,
+      breed: doc.breed,
+      city: doc.city,
+      province: doc.province
+    }
+    items.push(item);
+    i++;
+  };
+
+  res.render('index', {pretty: true, items: items});
 });
 
 module.exports = router;
