9561ffd1e6250bc142d4fea9410232616e68ffb4
Guoao Wei <guoao.wei@outlook.com>
2022-11-14 17:12:35 -0800

    Week 9: React elements, pass information between parents and children

--- week9/app/src/App.js
+++ week9/app/src/App.js
@@ -1,34 +1,16 @@
 import Header from "./components/Header";
-
-function addLi(task) {
-}
+import TaskList from "./components/TaskList";
 
 function App() {
   const appName = "Guoao's App";
 
-  const tasks = [
-    {
-      id: 1,
-      title: "Review week 9 material",
-      date: "June 4th at 1 pm",
-    },
-    {
-      id: 2,
-      title: "Do quiz 9",
-      date: "June 4th at 6 pm",
-    },
-    {
-      id: 3,
-      title: "Work on assignment 2",
-      date: "June 5th at 8 am",
-    },
-  ];
+
 
   return (
     <div className="App">
       <Header name={appName} />
       <ul>
-        {tasks.map((item) => { return <li>{item.title}</li>; })}
+        <TaskList />
       </ul>
     </div>
   );
---
+++ week9/app/src/components/TaskList.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Task from "./Task";
+
+
+function TaskList() {
+  const [tasks, userTasks] = useState([
+    {
+      id: 1,
+      title: "Review week 9 material",
+      date: "June 4th at 1 pm",
+    },
+    {
+      id: 2,
+      title: "Do quiz 9",
+      date: "June 4th at 6 pm",
+    },
+    {
+      id: 3,
+      title: "Work on assignment 2",
+      date: "June 5th at 8 am",
+    },
+  ]);
+
+  const deletePressed = (deletedId) => {
+    console.log("Clicked " + deletedId);
+  };
+
+  return (
+    <>
+      { tasks.map((item) => { return <Task key={item.id} task={item} deleteHandler={deletePressed} />; }) }
+    </>
+    );
+}
+
+export default TaskList;
